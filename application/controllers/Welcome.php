65cb2928c26138ea275c660b75777addb6ebe476
Vlad Iacobeț <Vlad Iacobe>
2016-05-02 11:52:02 +0300

    set database.php (this file handles the database connenction)

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,21 +5,17 @@ class Welcome extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
 	{
 		$this->load->view('welcome_message');
+		$this->load->model('Model');
+		$this->Model->test();
+		$this->load->database();
+		echo "<br>";
+		$query = $this->db->query('SELECT * FROM FaceMaze_Users');
+		foreach ($query->result_array() as $row) {
+		    echo $row['MAIL_ADDRESS'];
+		}
 	}
 }
